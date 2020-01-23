9e4179927ed230726ab46135166cc203c40144f2
Agnaldo Marinho <agnaldomarinho7@gmail.com>
2020-01-23 12:21:55 -0300

    Organizando o projeto por modulos e diretorios

---
+++ meat-api/server/server.ts
@@ -0,0 +1,52 @@
+import * as restify from 'restify';
+
+export class Server {
+
+    application: restify.Server
+
+    iniRoutes(): Promise<any>{
+        return new Promise((resolve, reject) => {
+            try{
+                this.application = restify.createServer({
+                    name: 'meat-api',
+                    version: '1.0.0'
+                })
+
+                this.application.use(restify.plugins.queryParser())
+
+                // Routes
+                this.application.get('/info', [
+                    (req, resp, next) =>{
+                        if(req.userAgent() && req.userAgent().includes('MSIE 7.0')){
+                            let error: any = new Error()
+                            error.statusCode = 400
+                            error.message = 'Please, update your brawser'
+                            return next(error)
+                        }
+                        return next()
+                    }, (req, resp, next) =>{
+                    resp.json({
+                        browser: req.userAgent(),
+                        method: req.method,
+                        url: req.href(),
+                        path: req.path(),
+                        query: req.query
+                    })
+                    return next()
+                }])
+
+
+                this.application.listen(3000, ()=>{
+                    resolve(this.application)
+                })
+
+            } catch(error){
+                reject(error)
+            }
+        })
+    }
+
+    bootstrap(): Promise<Server>{
+        return this.iniRoutes().then(() => this)
+    }
+}
